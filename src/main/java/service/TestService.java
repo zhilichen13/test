bd268ef84c66297a556194d207e4dc0444d5fca0
chenzhili <zhilichen13@zto.com>
2022-09-17 18:20:29 +0800

    src

---
+++ src/main/java/service/TestService.java
@@ -0,0 +1,11 @@
+package service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: zhilichen
+ * @Date: 2022/09/17/17:37
+ * @Description:
+ */
+public class TestService {
+}
---
+++ src/main/java/service/impl/TestServiceImpl.java
@@ -0,0 +1,11 @@
+package service.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: zhilichen
+ * @Date: 2022/09/17/17:38
+ * @Description:
+ */
+public class TestServiceImpl {
+}
